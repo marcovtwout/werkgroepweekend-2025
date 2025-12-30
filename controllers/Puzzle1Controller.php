54d4e39cbf848cba8d4924aac20479917d87d664
Marco van 't Wout <marco@tremani.nl>
2025-12-30 17:55:05 +0100

    Add puzzle 1

--- controllers/Puzzle1Controller.php
+++ controllers/Puzzle1Controller.php
@@ -2,7 +2,9 @@
 
 namespace app\controllers;
 
+use app\models\Log;
 use Yii;
+use yii\base\DynamicModel;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
@@ -31,6 +33,28 @@ class Puzzle1Controller extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        $model = new DynamicModel(['answer']);
+        $model->addRule('answer', 'required');
+        $model->addRule('answer', function ($attribute) {
+            $correct =
+                str_contains($this->$attribute, 'suske')
+                && str_contains($this->$attribute, 'wiske');
+
+            if (!$correct) {
+                $this->addError($attribute, 'Helaas!');
+            };
+        });
+
+        if ($model->load(Yii::$app->request->post())) {
+            Log::addEntry(Yii::$app->user->identity, 'puzzle1', $model->answer);
+            if ($model->validate()) {
+                // goed!
+                exit;
+            }
+        }
+
+        return $this->render('index', [
+            'model' => $model,
+        ]);
     }
 }
--- views/puzzle1/index.php
+++ views/puzzle1/index.php
@@ -1,4 +1,34 @@
 <?php
+
+use yii\base\DynamicModel;
+use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
+
+/** @var DynamicModel $model */
+
 ?>
 
-puzzle1
+<p>
+    Welkomstwoord<br>
+    De eerste puzzel moet je proberen om zelfstandig of binnen je huishouden op te lossen.<br>
+    Deze uitdaging is met de hand en met liefde voor jullie gemaakt, zonder gebruik te maken van AI. Dus die heb je ook niet nodig.<br>
+    Hulpmiddelen: huishouden of direct familie mag helpen. géén internet/zoekmachine/AI gebruiken<br>
+</p>
+
+<div class="row mt-5">
+    <div class="col-sm-6 offset-sm-3">
+        <?php $form = ActiveForm::begin([
+            'enableClientValidation' => false,
+        ]); ?>
+
+        <?= $form->field($model, 'answer')->textInput(['autofocus' => true])->label('Antwoord') ?>
+
+        <div class="form-group">
+            <div>
+                <?= Html::submitButton('Inloggen', ['class' => 'btn btn-primary']) ?>
+            </div>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
