51a0e492d4106ef16c8afaaf40bdd4c0d1418ec4
Marco van 't Wout <marco@tremani.nl>
2025-12-30 22:33:10 +0100

    Add import quiz command

---
+++ commands/QuizController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\commands;
+
+use app\models\QuizAnswer;
+use app\models\QuizQuestion;
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+class QuizController extends Controller
+{
+    public function actionImport()
+    {
+        $data = require __DIR__ . '/quizData.php';
+
+        QuizQuestion::deleteAll();
+
+        foreach($data as $index => $row) {
+            $questionTitle = array_shift($row);
+            $answers = $row;
+
+            $question = new QuizQuestion();
+            $question->nr = $index + 1;
+            $question->title = $questionTitle;
+            $question->save();
+
+            foreach($answers as $answerIndex => $answerText) {
+                $answer = new QuizAnswer();
+                $answer->questionId = $question->id;
+                $answer->text = trim(str_replace('[Correct]', '', $answerText));
+                $answer->isCorrect = str_contains($answerText, '[Correct]');
+                $answer->position = $answerIndex+1;
+                $answer->save();
+            }
+        }
+
+        return ExitCode::OK;
+    }
+}
